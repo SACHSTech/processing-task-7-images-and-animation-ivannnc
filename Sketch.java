1591cd8fb3d9b0748d0b8172d6d81317884beb3a
ivannnc <junxian.chen24@ycdsbk12.ca>
2023-11-20 18:00:16 -0500

    submit

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,28 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+	PImage imgBackground;
+  PImage imgFloatingCow;
 	
-	
+  // state image and background
+  float fltBackgroundX = 0;
+  float fltBackgroundY = 0;
+  float fltCowX = 0;
+  float fltCowY = 0;
+  float cowXSpeed = 2;
+  float cowYSpeed = 2;
+
+  // state circle
+  float circleX = 50;
+  float circleY = 0;
+  float xSpeed = 2;
+  float ySpeed = 2;
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
-	// put your size call here
     size(400, 400);
   }
 
@@ -16,21 +31,51 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    // state image and background generation 
+    background(0);
+    imgBackground = loadImage("Sample.png");
+    imgFloatingCow = loadImage("cowAgain-removebg-preview.png");
+
+    
+
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+  // generate background and image
+	 image(imgBackground, fltBackgroundX, fltBackgroundY);
+   image(imgFloatingCow, fltCowX, fltCowY);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+  // animate cow image
+    fltCowX = fltCowX + cowXSpeed;
+    fltCowY = fltCowY + cowYSpeed;
+
+    if (fltCowX < 0 || fltCowX > 200){
+      cowXSpeed = cowXSpeed *- 1;
+    }
+    else if (fltCowY < 0 || fltCowY > 200){
+      cowYSpeed = cowYSpeed *- 1;
+    }
+
+    
+  // generate cow's toy
+    fill(51, 204, 255);
+    ellipse(circleX, circleY, 25, 25);
+
+  // modify circle
+    circleX = circleX + xSpeed;
+    circleY = circleY + ySpeed;
+
+  // bounce in all directions
+    if(circleX < 0 || circleX > height){
+      xSpeed = xSpeed *- 1;
+    }
+    else if (circleY < 0 || circleY > height){
+      ySpeed = ySpeed *- 1;
+    }
+
   }
-  
   // define other methods down here.
 }
